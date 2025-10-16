09a225108b697e93b3270bf3c2561eace7a3c1d0
Speefak <63617780+speefak@users.noreply.github.com>
2025-10-16 13:44:24 +0200

    Update SSHFS user and host configuration

--- rear-backup_v0.1.9.sh
+++ rear-backup_v0.1.9.sh
@@ -17,8 +17,9 @@
  ScriptName=$(basename $0)
 
 # TODO userinput / configfile
- SSHFSUser=user
- SSHFSHost=ip
+ SSHFSUser=speefak
+ SSHFSHost=192.168.1.x
+ 
  BackupFileSuffix="OS"
  OutputDirectoryOverride="true"
  TmpDirectoryOverride="false"			# does not work on debian 12 => recovery error => mktemp:failed to create file via template 'home/rear_tmp/tmp.XXXXXXXXXX: No such file ....
